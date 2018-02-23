8c18a41056bf7a0cb16c8ca6abcc74e7e3a03225
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-23 15:49:22 -0600

    Created Manifest class

---
+++ Sources/Manifest/Manifest.swift
@@ -0,0 +1,8 @@
+/// An interface for a package's manifest.
+public class Manifest {
+    
+    /// Gets the current project's manifest.
+    public static let current = Manifest()
+    
+    private init() {}
+}
