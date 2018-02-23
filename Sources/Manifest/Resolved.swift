08210670141801ae4b8aaa9951e91ee052dca089
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-23 16:25:03 -0600

    Changed Manifest.resolved return type to Resolved

--- Sources/Manifest/Resolved.swift
+++ Sources/Manifest/Resolved.swift
@@ -2,14 +2,15 @@ import Foundation
 
 extension Manifest {
     
-    /// Gets the contents of the package's `Package.resolved` file as `Data`.
+    /// Gets the contents of the package's `Package.resolved` file as a data structure.
     ///
     /// - Returns: The contents of the packages `.resolved` file.
-    /// - Throws: `ManifestError.badPath` if the path to the `.resolved` file is malformed.re
-    func resolved()throws -> Data {
+    /// - Throws: `ManifestError.badPath` if the path to the `.resolved` file is malformed.
+    func resolved()throws -> Resolved {
         guard let resolvedURL = URL(string: "file:\(fileManager.currentDirectoryPath)/Package.resolved") else {
             throw ManifestError(identifier: "badPath", reason: "Bad path to package data. Make sure you are in the project root.")
         }
-        return try Data(contentsOf: resolvedURL)
+        let resolved = try Data(contentsOf: resolvedURL)
+        return try JSONDecoder().decode(Resolved.self, from: resolved)
     }
 }
