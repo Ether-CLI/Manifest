c03dc54f14d3dc937a704b966048c4b1a5134c19
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-23 18:46:15 -0600

    Moved Manifest.targets method to Targets.get

--- Sources/Manifest/Target.swift
+++ Sources/Manifest/Target.swift
@@ -12,35 +12,7 @@ public struct Target: Codable {
 extension Manifest {
     
     /// An interface for the package's target declarations.
-    var targets: Targets { return Targets() }
-    
-    /// Gets the targets declared in the project's manifest.
-    ///
-    /// - warning: Not complete. Only gets target name and dependencies.
-    ///
-    /// - returns: The project's targets parsed into data structures.
-    /// - throws: Errors that occur while fetching the manifest contents or create the regex for maching the targets.
-    public func targets()throws -> [Target] {
-        let pattern = try NSRegularExpression(
-            pattern: "\\.(testT|t)arget\\(name:\\s*\"(.*?)\",\\s*dependencies:\\s*(\\[.*?\\])\\)",
-            options: []
-        )
-        let contents: String = try self.contents()
-        let string: NSString = NSString(string: contents)
-        
-        return pattern.matches(in: contents, range: NSRange(contents.startIndex..., in: contents)).map { (result) -> Target in
-            let nameRange = result.range(at: 2)
-            let dependenciesRange = result.range(at: 3)
-            
-            let name = string.substring(with: nameRange)
-            let dependenciesString: String = String(string.substring(with: dependenciesRange).dropFirst().dropLast())
-            let dependencies = dependenciesString.split(separator: ",").map({ (dep) in
-                return String(dep.trimmingCharacters(in: .whitespacesAndNewlines).dropLast().dropFirst())
-            })
-            
-            return Target(name: name, path: nil, publicHeadersPath: nil, dependencies: dependencies, exclude: [], source: [])
-        }
-    }
+    var targets: Targets { return Targets(manifest: self) }
     
     /// Gets the target declaration with a given name
     ///
@@ -57,5 +29,38 @@ extension Manifest {
 
 /// A wrapper for interacting with manifest target declarations.
 public class Targets {
-    init() {}
+    
+    private let manifest: Manifest
+    
+    public init(manifest: Manifest) {
+        self.manifest = manifest
+    }
+    
+    /// Gets the targets declared in the project's manifest.
+    ///
+    /// - warning: Not complete. Only gets target name and dependencies.
+    ///
+    /// - returns: The project's targets parsed into data structures.
+    /// - throws: Errors that occur while fetching the manifest contents or create the regex for maching the targets.
+    public func get()throws -> [Target] {
+        let pattern = try NSRegularExpression(
+            pattern: "\\.(testT|t)arget\\(name:\\s*\"(.*?)\",\\s*dependencies:\\s*(\\[.*?\\])\\)",
+            options: []
+        )
+        let contents: String = try manifest.contents()
+        let string: NSString = NSString(string: contents)
+        
+        return pattern.matches(in: contents, range: NSRange(contents.startIndex..., in: contents)).map { (result) -> Target in
+            let nameRange = result.range(at: 2)
+            let dependenciesRange = result.range(at: 3)
+            
+            let name = string.substring(with: nameRange)
+            let dependenciesString: String = String(string.substring(with: dependenciesRange).dropFirst().dropLast())
+            let dependencies = dependenciesString.split(separator: ",").map({ (dep) in
+                return String(dep.trimmingCharacters(in: .whitespacesAndNewlines).dropLast().dropFirst())
+            })
+            
+            return Target(name: name, path: nil, publicHeadersPath: nil, dependencies: dependencies, exclude: [], source: [])
+        }
+    }
 }
