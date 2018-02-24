a30aa00b5acc45b8ea8b82fd8a3d0963ed4a9c49
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-23 18:36:00 -0600

    Made resolved pins data structure types public

--- Sources/Manifest/Pin.swift
+++ Sources/Manifest/Pin.swift
@@ -1,42 +1,42 @@
 /// The full structure of a `Package.resolved` file.
-struct Resolved: Codable {
+public struct Resolved: Codable {
     
     /// The value for the `.resolved` JSON's `object` key.
-    let object: Pins
+    public let object: Pins
     
     /// Not sure what this is for, but it exists.
-    let version: Int
+    public let version: Int
 }
 
 /// Holds the array of dependency pins.
-struct Pins: Codable {
+public struct Pins: Codable {
     
     /// The pins for the projects dependecnies.
-    let pins: [Pin]
+    public let pins: [Pin]
 }
 
 /// A pin for a dependedny.
-struct Pin: Codable {
+public struct Pin: Codable {
     
     /// The name of the package.
-    let package: String
+    public let package: String
     
     /// The `.git` URL of the dependency.
-    let repositoryURL: String
+    public let repositoryURL: String
     
     /// The version data of the dependecy pulled into the dependeny.
-    let state: PinState
+    public let state: PinState
 }
 
 /// The version data for a dependeny pin.
-struct PinState: Codable {
+public struct PinState: Codable {
     
     /// The branch of the depenency that is being used.
-    let branch: String?
+    public let branch: String?
     
     /// The current git revision of the dependency that was fetched.
-    let revision: String
+    public let revision: String
     
     /// The current git tag of the dependency that is pulled into the project.
-    let version: String?
+    public let version: String?
 }
