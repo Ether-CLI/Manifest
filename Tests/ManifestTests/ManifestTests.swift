767c3722c56e0cfa70cd1c5bc230d7b7b599e53a
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-02-28 16:31:09 -0600

    Import Utilities to ManifetsTests.swift

--- Tests/ManifestTests/ManifestTests.swift
+++ Tests/ManifestTests/ManifestTests.swift
@@ -1,5 +1,6 @@
 import XCTest
 @testable import Manifest
+@testable import Utilities
 
 class ManifestTests: XCTestCase {
     static var allTests: [(String, (ManifestTests)throws -> () -> ())] = []
