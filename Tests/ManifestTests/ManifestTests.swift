52ded9ea214edae07d890a5a19e9c6176a29598d
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-01 16:30:02 -0600

    Added testing string to ManifestTest.swift

--- Tests/ManifestTests/ManifestTests.swift
+++ Tests/ManifestTests/ManifestTests.swift
@@ -5,3 +5,26 @@ import XCTest
 class ManifestTests: XCTestCase {
     static var allTests: [(String, (ManifestTests)throws -> () -> ())] = []
 }
+
+let fakeManifest = """
+// swift-tools-version:4.0
+
+import PackageDescription
+
+let package = Package(
+    name: "Ether",
+    products: [
+        .executable(name: "Ether", targets: ["pomPom", "GorGit", "Flute", "Ether", "banzi", "Kung-Fu"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/vapor/console.git", .exact("2.3.0")),
+        .package(url: "https://github.com/vapor/json.git", .exact("2.2.1")),
+        .package(url: "https://github.com/vapor/core.git", .exact("2.2.0"))
+    ],
+    targets: [
+        .target(name: "Helpers", dependencies: ["Core", "JSON"]),
+        .target(name: "Ether", dependencies: ["Helpers", "Console", "JSON"]),
+        .target(name: "Executable", dependencies: ["Ether"])
+    ]
+)
+"""
