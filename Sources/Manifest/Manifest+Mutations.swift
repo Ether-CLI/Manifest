e1f975046e3e3089e9e81776727b9b3d2ceb693d
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-24 15:16:12 -0500

    Created Manifest.packageConfig() method

--- Sources/Manifest/Manifest+Mutations.swift
+++ Sources/Manifest/Manifest+Mutations.swift
@@ -27,4 +27,19 @@ extension Manifest {
             return content.substring(with: match.range(at: 2))
         }
     }
+    
+    /// Gets the value of the `Package.pkgConfig` property.
+    ///
+    /// - Returns: `Package.pkgConfig`
+    /// - Throws: Errors that occur when creating a RegEx pattern
+    ///   or reading or writing the manifest.
+    func packageConfig()throws -> String? {
+        let pattern = try NSRegularExpression(pattern: "\\n?\\s*pkgConfig:\\s*\"(.*?)\",?", options: [])
+        let content: NSMutableString = try self.contents()
+        
+        guard let match = pattern.matches(in: String(content), options: [], range: content.range).first else {
+            throw ManifestError(identifier: "missingManifestName", reason: "Attempted to access package name, but none where found")
+        }
+        return content.substring(with: match.range(at: 1))
+    }
 }
