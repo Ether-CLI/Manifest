5bc3dd4a1b683a7937e04c9070283227d1733573
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-24 14:41:04 -0500

    Changed Target argument properties from constants to variables

--- Sources/Manifest/Target/Target.swift
+++ Sources/Manifest/Target/Target.swift
@@ -8,22 +8,22 @@ public class Target {
     public let isTest: Bool
     
     /// The name of the target.
-    public let name: String
+    public var name: String
     
     /// The path to the target from the root of the project.
-    public let path: String?
+    public var path: String?
     
     /// The path to the directory containing public headers of a C target. _Only valid for C family library targets._
-    public let publicHeadersPath: String?
+    public var publicHeadersPath: String?
     
     /// The targets in the package or one of the packages dependencies that the declared target can access through imports.
-    public let dependencies: [String]
+    public var dependencies: [String]
     
     /// The files and directories that are excluded from being picked up as sources.
-    public let exclude: [String]
+    public var exclude: [String]
     
     /// The source files to include in the target. If it is empty, then any valid source file is included.
-    public let source: [String]
+    public var source: [String]
     
     /// The `Targets` instance that the target originates from.
     public let manifest: Manifest
