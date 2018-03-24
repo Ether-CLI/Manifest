676f4677a08b308a58e2d35ba494f3b2e8959cd6
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-24 15:54:41 -0500

    Made Provider type public

--- Sources/Manifest/Providers/Providers.swift
+++ Sources/Manifest/Providers/Providers.swift
@@ -4,14 +4,14 @@ import Utilities
 /// Respresents a system package manager and the packages that should be installed through that given package manager.
 ///
 /// More information [here](https://github.com/apple/swift-package-manager/blob/master/Documentation/PackageDescriptionV4.md#providers)
-struct Provider {
+public struct Provider {
     
     /// The packages to suggest installation for.
-    var packages: [String]
+    public var packages: [String]
     
     /// The manifest instance used to mutate or fetch
     /// information from the project's manifest.
-    let manifest: Manifest
+    private let manifest: Manifest
     
     ///
     private init(packages: [String], manifest: Manifest? = nil) {
