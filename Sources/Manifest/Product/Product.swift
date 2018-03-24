ef0e422487b082f6069abaa37ba782fdfd86de57
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-24 17:04:46 -0500

    Created Product type

--- Sources/Manifest/Product/Product.swift
+++ Sources/Manifest/Product/Product.swift
@@ -1,2 +1,28 @@
 import Foundation
 import Utilities
+
+/// A product that is vended by the parent package.
+///
+/// For more information, visit the [SPM docs](https://github.com/apple/swift-package-manager/blob/master/Documentation/PackageDescriptionV4.md#products).
+public final class Product {
+    
+    /// Denotes wheather the product is a library or an executable.
+    public let type: ProductType
+    
+    /// The name of the target.
+    public var name: String
+    
+    /// How the product gets linked to a client project if it is a library.
+    public var linking: LibraryLinkingType?
+    
+    /// The package's targets that the product can access.
+    public var targets: [String]
+    
+    ///
+    init(type: ProductType, name: String, linking: LibraryLinkingType?, targets: [String]) {
+        self.type = type
+        self.name = name
+        self.linking = linking
+        self.targets = targets
+    }
+}
