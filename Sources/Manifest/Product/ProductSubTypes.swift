ec219da146db3c83792a6d5d16ad1a4930865466
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-24 16:55:38 -0500

    Created ProductType enum

---
+++ Sources/Manifest/Product/ProductSubTypes.swift
@@ -0,0 +1,11 @@
+/// Defines what type of a `Product` object
+/// for the project's manifest.
+public enum ProductType: String {
+    
+    ///
+    case library
+    
+    ///
+    case executable
+}
+
