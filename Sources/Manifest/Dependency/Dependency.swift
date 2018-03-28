18de0178e304d09ed6cf9c46a13754b69b9b833c
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-28 16:05:44 -0500

    Created Manifest.dependencies method

--- Sources/Manifest/Dependency/Dependency.swift
+++ Sources/Manifest/Dependency/Dependency.swift
@@ -76,3 +76,27 @@ extension Dependency: Saveable {
         try self.manifest.write(with: contents)
     }
 }
+
+extension Manifest {
+    
+    /// Get all the elements in `Package.dependencies` array from the project's manifest.
+    ///
+    /// - Returns: `Package.dependencies`
+    /// - Throws: Errors that occur when creating a RegEx pattern
+    ///   or reading or writing the manifest.
+    public func dependencies()throws -> [Dependency] {
+        let pattern = try NSRegularExpression(pattern: "\\.package\\(\\s*url\\s*:\\s*\"(.*?)\"\\s*,\\s*(.*?)\\)(?=\\s*,|\\s*\\])", options: [])
+        let contents: String = try self.contents()
+        
+        return try pattern.matches(in: contents, options: [], range: contents.range).map { (match) in
+            guard let url = contents.substring(at: match.range(at: 1)) else {
+                throw ManifestError.noStringAtRange(match.range(at: 1))
+            }
+            guard let version = contents.substring(at: match.range(at: 2)) else {
+                throw ManifestError.noStringAtRange(match.range(at: 2))
+            }
+            
+            return try Dependency(manifest: self, url: url, version: version)
+        }
+    }
+}
