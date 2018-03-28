c8a3238de46522e37e1c6c129de48bd0dddaec0c
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-28 16:56:45 -0500

    Made Environment properties public

--- Sources/Manifest/Environment.swift
+++ Sources/Manifest/Environment.swift
@@ -2,12 +2,12 @@
 public struct Environment: Equatable {
     
     /// The name of the environmnt
-    let name: String
+    public let name: String
     
     
     /// Used to tell the manifest instance that it is being used in the command-line.
-    static let commandline = Environment(name: "command-line")
+    public static let commandline = Environment(name: "command-line")
     
     /// Used to tell the manifest it is being used to test it's methods on a string.
-    static let testing = Environment(name: "testing")
+    public static let testing = Environment(name: "testing")
 }
