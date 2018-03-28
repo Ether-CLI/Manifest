1b1c579c1e8c942b785637a493d5bcfffe098b21
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-28 16:49:38 -0500

    Created Manifest.prooduct method

--- Sources/Manifest/Product/Product.swift
+++ Sources/Manifest/Product/Product.swift
@@ -53,29 +53,3 @@ public final class Product: CustomStringConvertible {
         ")"
     }
 }
-
-extension Manifest {
-    
-    /// Gets all the `Product` instances in the current manifest.
-    public func products()throws -> [Product] {
-        let pattern = try NSRegularExpression(
-            pattern: "\\.(?:executable|library)\\(name\\s*:\\s*\"(.*?)\"\\s*(?:,\\s*type\\s*:\\s*\\.(static|dynamic)\\s*)?(?:,\\s*targets\\s*:\\s*(\\[.*?\\]))?",
-            options: []
-        )
-        let contents: String = try self.contents()
-        
-        return try pattern.matches(in: contents, options: [], range: contents.range).map({ (match) in
-            guard let raw = contents.substring(at: match.range(at: 1)), let type = ProductType(rawValue: raw) else {
-                throw ManifestError.noStringAtRange(match.range(at: 1))
-            }
-            guard let name = contents.substring(at: match.range(at: 2)) else {
-                throw ManifestError.noStringAtRange(match.range(at: 2))
-            }
-            
-            let linking = LibraryLinkingType(rawValue: contents.substring(at: match.range(at: 3)) ?? "")
-            let targets = contents.parseArray(at: match.range(at: 4))
-            
-            return Product(from: self, withType: type, name: name, linking: linking, targets: targets)
-        })
-    }
-}
