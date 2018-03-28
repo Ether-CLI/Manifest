bae1ee74e20700f8d2fc6ef0071a29613fac34e1
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-28 16:44:17 -0500

    Created Manifest.dependency(withURL:) method

--- Sources/Manifest/Dependency/Dependency.swift
+++ Sources/Manifest/Dependency/Dependency.swift
@@ -34,27 +34,3 @@ public final class Dependency: CustomStringConvertible {
         return ".package(url: \"\(self.url)\", \(self.version.description))"
     }
 }
-
-extension Manifest {
-    
-    /// Get all the elements in `Package.dependencies` array from the project's manifest.
-    ///
-    /// - Returns: `Package.dependencies`
-    /// - Throws: Errors that occur when creating a RegEx pattern
-    ///   or reading or writing the manifest.
-    public func dependencies()throws -> [Dependency] {
-        let pattern = try NSRegularExpression(pattern: "\\.package\\(\\s*url\\s*:\\s*\"(.*?)\"\\s*,\\s*(.*?)\\)(?=\\s*,|\\s*\\])", options: [])
-        let contents: String = try self.contents()
-        
-        return try pattern.matches(in: contents, options: [], range: contents.range).map { (match) in
-            guard let url = contents.substring(at: match.range(at: 1)) else {
-                throw ManifestError.noStringAtRange(match.range(at: 1))
-            }
-            guard let version = contents.substring(at: match.range(at: 2)) else {
-                throw ManifestError.noStringAtRange(match.range(at: 2))
-            }
-            
-            return try Dependency(manifest: self, url: url, version: version)
-        }
-    }
-}
