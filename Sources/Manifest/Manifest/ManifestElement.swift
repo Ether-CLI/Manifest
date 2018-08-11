9aa1cf2f86fad8d4849825ec909d6cf5d920beac
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-08-11 11:17:46 -0500

    Made ManifestElement a class protocol

--- Sources/Manifest/Manifest/ManifestElement.swift
+++ Sources/Manifest/Manifest/ManifestElement.swift
@@ -1,6 +1,6 @@
 
 /// A type that can be written to a manifest as a value to initialize a `Package` instance.
-public protocol ManifestElement {
+public protocol ManifestElement: class {
     
     /// The parent manifest of the element.
     var manifest: Manifest { get set }
