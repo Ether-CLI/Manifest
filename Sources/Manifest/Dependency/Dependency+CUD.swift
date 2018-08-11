dabdc2b5ba083e81a75a0be81d7928f6e52a1037
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-08-11 12:06:04 -0500

    Fix whitespace when adding first deoendency to manifest. Allow last dependency to have trailing comma

--- Sources/Manifest/Dependency/Dependency+CUD.swift
+++ Sources/Manifest/Dependency/Dependency+CUD.swift
@@ -11,23 +11,35 @@ extension Dependency: Saveable {
     public func save() throws {
         let contents: NSMutableString = try self.manifest.contents()
         
-        if try NSRegularExpression(
+        let dependencies = try NSRegularExpression(
+            pattern: "(Package\\(\\n?(\\s*)name\\s*:\\s*\".*?\"\\s*,?\\s*(providers\\s*:\\s*\\[(.|\\n)*?\\](?!\\s*\\)),?\\s*)?(products\\s*:\\s*\\[(.|\\n)*?\\](?!\\s*\\)),?\\s*)?(\\n?(\\s*)dependencies\\s*:\\s*\\[)(\\n?\\s*))(\\]|(.|\\n)*?\\),?\\s*\\])",
+            options: []
+        )
+        let array = try NSRegularExpression(
             pattern: "Package\\(\\n?(\\s*)name\\s*:\\s*\".*?\"\\s*,?\\s*(providers\\s*:\\s*\\[(.|\\n)*?\\](?!\\s*\\)),?\\s*)?(products\\s*:\\s*\\[(.|\\n)*?\\](?!\\s*\\)),?\\s*)?dependencies",
             options: []
-            ).matches(in: String(contents), options: [], range: contents.range).count > 0 {
-            let stored = try NSRegularExpression(pattern: "\\.package\\(url\\s*:\\s*\"\(self.url)\".*?\\)(?=\\s*,|\\s*\\])", options: [])
+        )
+        let before = try NSRegularExpression(
+            pattern: "(Package\\(\\n?(\\s*)name\\s*:\\s*\".*?\"\\s*,?\\s*(providers\\s*:\\s*\\[(.|\\n)*?\\](?!\\s*\\)),?\\s*)?(products\\s*:\\s*\\[(.|\\n)*?\\](?!\\s*\\)))?)(,)?",
+            options: []
+        )
+        let stored = try NSRegularExpression(pattern: "\\.package\\(url\\s*:\\s*\"\(self.url)\".*?\\)(?=\\s*,|\\s*\\])", options: [])
+        
+        if array.matches(in: String(contents), options: [], range: contents.range).count > 0 {
             if stored.matches(in: String(contents), options: [], range: contents.range).count > 0 {
                 stored.replaceMatches(in: contents, options: [], range: contents.range, withTemplate: self.description)
             } else {
-                let pattern = try NSRegularExpression(pattern: "(Package\\(\\n?(\\s*)name\\s*:\\s*\".*?\"\\s*,?\\s*(providers\\s*:\\s*\\[(.|\\n)*?\\](?!\\s*\\)),?\\s*)?(products\\s*:\\s*\\[(.|\\n)*?\\](?!\\s*\\)),?\\s*)?(\\n?(\\s*)dependencies\\s*:\\s*\\[)(\\n?\\s*))(\\]|(.|\\n)*?\\)\\s*\\])", options: [])
-                pattern.replaceMatches(in: contents, options: [], range: contents.range, withTemplate: "$1\(self.description),$9$10")
+                let match = dependencies.firstMatch(in: String(contents), options: [], range: contents.range)
+                let space = contents.substring(with: match?.range(at: 9) ?? NSMakeRange(0, 0))
+                
+                if space == "" {
+                    dependencies.replaceMatches(in: contents, options: [], range: contents.range, withTemplate: "$1\n$2$2\(self.description)\n$2]")
+                } else {
+                    dependencies.replaceMatches(in: contents, options: [], range: contents.range, withTemplate: "$1\(self.description),$9$10")
+                }
             }
         } else {
-            let pattern = try NSRegularExpression(
-                pattern: "(Package\\(\\n?(\\s*)name\\s*:\\s*\".*?\"\\s*,?\\s*(providers\\s*:\\s*\\[(.|\\n)*?\\](?!\\s*\\)),?\\s*)?(products\\s*:\\s*\\[(.|\\n)*?\\](?!\\s*\\)))?)(,)?",
-                options: []
-            )
-            pattern.replaceMatches(in: contents, options: [], range: contents.range, withTemplate: """
+            before.replaceMatches(in: contents, options: [], range: contents.range, withTemplate: """
                 $1,
                 $2dependenciesw: [
                 $2$2\(self.description)
